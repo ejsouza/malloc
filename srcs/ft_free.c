3368fc76ca2a0250ceff9afbe19dbcbe2e48a703
Eder De Souza <esouza@student.42.fr>
2020-02-17 13:32:55 +0100

    Clean free()
    Cleaned the file ft_free.c from comments and prints

--- srcs/ft_free.c
+++ srcs/ft_free.c
@@ -17,7 +17,6 @@ static void     bzero_chunk(t_chunk *chunk)
     chunk->next = NULL;
     chunk->prev = NULL;
     chunk->free = 1;
-    // chunk->size = 0;
 }
 
 static void     coalesce(t_chunk *curr, t_chunk *neighbor)
@@ -33,14 +32,12 @@ static void     coalesce(t_chunk *curr, t_chunk *neighbor)
             tmp->prev = start;
         start->free = 1;
         start->size += (neighbor->size + sizeof(t_chunk));
-       // printf("coalesce(next) curr->size %zu | %zu\n", start->size, neighbor->size);
         start->next = (void *)tmp;
         bzero_chunk(neighbor);
     }
     else if (curr > neighbor) // prev
     {
         start = neighbor;
-     //   printf("coalesce(prev) curr->size %zu | %zu | sizeof(t_chunk) %zu\n", start->size, neighbor->size, sizeof(t_chunk));
         if ((tmp = (t_chunk *)curr->next) != NULL)
             tmp->prev = start;
         start->free = 1;
@@ -48,7 +45,6 @@ static void     coalesce(t_chunk *curr, t_chunk *neighbor)
         start->next = (void *)tmp;
         bzero_chunk(curr);
     }
-    // update_size_block_head(start);
 }
 
 static void     free_ptr(t_chunk *ptr)
@@ -61,9 +57,7 @@ static void     free_ptr(t_chunk *ptr)
     while (curr->prev != NULL)
         curr = curr->prev;
     head_block = (t_block *)curr - ONE;
-   // printf("free_ptr(%zu)[%zu]\t head_block %zu\n", ptr->size, ptr->size - sizeof(t_chunk), head_block->blc_size);
-    head_block->blc_size += ptr->size;
-   // printf("free_ptr(%zu)[%zu]\t head_block %zu\n", ptr->size, ptr->size - sizeof(t_chunk), head_block->blc_size);
+    head_block->blc_size += ptr->size + sizeof(t_chunk);
 }
 
 static int     neighbor_is_free(t_chunk *ptr)
@@ -96,44 +90,26 @@ static int     neighbor_is_free(t_chunk *ptr)
 void            ft_free(void *ptr)
 {
     t_chunk     *curr;
+    t_chunk     *next;
     t_chunk     *tmp;
     t_block     *block_head;
     size_t      size_ptr_to_free;
     
-    printf("START FREE\n");
     if (ptr == NULL)
-    {
-        printf("NULL\n");
-        printf("END FREE\n");
         return ;
-    }
     curr = (t_chunk *)ptr - ONE;
+    next = curr;
     if (curr->free != 0)
-    {
-        printf("ERROR? %d\t SIZE %zu\n", curr->free, curr->size);
-        printf("END FREE\n");
         return ;
-    }
     size_ptr_to_free = curr->size;
     tmp = curr;
     while (tmp->prev != NULL)
-    {
         tmp = tmp->prev;
-    }
     if (!neighbor_is_free(curr))
-    {
-        printf("calling free_ptr(%p)\n", curr);
         free_ptr(curr);
-    }
-    /*
-    ** Probably down here the test for freeing the BLOCK
-    */
-    //printf("MIDLE FREE\n");
- 
+    while (next->next != NULL)
+        next = (t_chunk *)next->next;
     block_head = (t_block *)tmp - ONE;
-    //printf("TOTO FREE\n");
-    printf("%p size of block %zu\n", block_head, block_head->blc_size);
-    //printf("TATA FREE\n");
-    //free_block(block_head, size_ptr_to_free);
-    printf("END FREE\n");
+    if (next->size < block_head->blc_size)
+        next->size = block_head->blc_size;
 }
